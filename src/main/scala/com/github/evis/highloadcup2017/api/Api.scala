15653429a8d4e416968a1f63ac2ee5f086f9b84c
Evgeny Veretennikov <evg.veretennikov@gmail.com>
2017-08-16 21:01:15 +0300

    404 exception handler

--- src/main/scala/com/github/evis/highloadcup2017/api/Api.scala
+++ src/main/scala/com/github/evis/highloadcup2017/api/Api.scala
@@ -1,9 +1,19 @@
 package com.github.evis.highloadcup2017.api
 
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model.StatusCodes.NotFound
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.{ExceptionHandler, Route}
+import com.github.evis.highloadcup2017.dao.NotFoundException
 
 import scala.concurrent.ExecutionContext
 
 class Api(userApi: UserApi) {
-  def route(implicit ec: ExecutionContext): Route = userApi.route
+  def route(implicit ec: ExecutionContext): Route = handleExceptions(exceptionHandler) {
+    userApi.route
+  }
+
+  val exceptionHandler = ExceptionHandler {
+    case _: NotFoundException =>
+      complete(NotFound)
+  }
 }
