b03e7a4427bdf2d421a8a401f9cb2853d8be1d5f
Evgeny Veretennikov <evg.veretennikov@gmail.com>
2017-08-18 18:54:08 +0300

    Remove needless Future in InitialDataLoader

--- src/main/scala/com/github/evis/highloadcup2017/InitialDataLoader.scala
+++ src/main/scala/com/github/evis/highloadcup2017/InitialDataLoader.scala
@@ -7,31 +7,26 @@ import com.github.evis.highloadcup2017.dao.UserDao
 import com.github.evis.highloadcup2017.model.User
 import spray.json._
 
-import scala.concurrent.{ExecutionContext, Future}
-
 class InitialDataLoader(userDao: UserDao) {
-  def load(zipPath: String)(implicit ec: ExecutionContext): Future[Unit] = {
-    Future {
-      File(zipPath).unzip().list.foreach { file =>
-        // streaming?
-        val string = new String(Files.readAllBytes(file.path))
-        val (entityType, entitiesValue) = string.parseJson.asJsObject.fields.head
-        val entities = entitiesValue match {
-          case JsArray(elements) => elements
-          case other => throw new Exception(s"Bad entities. Expected JsArray, got $other")
-        }
-        val saveFun = entityType match {
-          case "users" => saveUsers _
-          case "locations" => saveLocations _
-          case "visits" => saveVisits _
-          case other => throw new Exception(s"Unknown entity type: $other")
-        }
-        saveFun(entities.map(_.asJsObject))
+  def load(zipPath: String): Unit =
+    File(zipPath).unzip().list.foreach { file =>
+      // streaming?
+      val string = new String(Files.readAllBytes(file.path))
+      val (entityType, entitiesValue) = string.parseJson.asJsObject.fields.head
+      val entities = entitiesValue match {
+        case JsArray(elements) => elements
+        case other => throw new Exception(s"Bad entities. Expected JsArray, got $other")
       }
+      val saveFun = entityType match {
+        case "users" => saveUsers _
+        case "locations" => saveLocations _
+        case "visits" => saveVisits _
+        case other => throw new Exception(s"Unknown entity type: $other")
+      }
+      saveFun(entities.map(_.asJsObject))
     }
-  }
 
-  private def saveUsers(jsons: Seq[JsObject])(implicit ec: ExecutionContext) {
+  private def saveUsers(jsons: Seq[JsObject]) {
     jsons.foreach(json => userDao.create(json.convertTo[User]))
   }
 
