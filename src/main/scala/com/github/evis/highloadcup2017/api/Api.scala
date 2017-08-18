3a6df258340429ecbbb4c35ac58c418daf0c6bec
Evgeny Veretennikov <evg.veretennikov@gmail.com>
2017-08-18 17:46:50 +0300

    Remove redundant NotFoundException

--- src/main/scala/com/github/evis/highloadcup2017/api/Api.scala
+++ src/main/scala/com/github/evis/highloadcup2017/api/Api.scala
@@ -1,19 +1,10 @@
 package com.github.evis.highloadcup2017.api
 
-import akka.http.scaladsl.model.StatusCodes.NotFound
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{ExceptionHandler, Route}
-import com.github.evis.highloadcup2017.dao.NotFoundException
+import akka.http.scaladsl.server.Route
 
 import scala.concurrent.ExecutionContext
 
 class Api(userApi: UserApi) {
-  def route(implicit ec: ExecutionContext): Route = handleExceptions(exceptionHandler) {
+  def route(implicit ec: ExecutionContext): Route =
     userApi.route
-  }
-
-  val exceptionHandler = ExceptionHandler {
-    case _: NotFoundException =>
-      complete(NotFound)
-  }
 }
--- src/main/scala/com/github/evis/highloadcup2017/dao/NotFoundException.scala
+++
@@ -1,5 +0,0 @@
-package com.github.evis.highloadcup2017.dao
-
-class NotFoundException(message: String = null,
-                        cause: Throwable = null)
-  extends Exception(message, cause)
